46199d3034b753bc7278e0d82d9d61f436cf051a
DosmirK <dosmir017@gmail.com>
2023-10-03 21:57:49 +0600

    Home_Work_2_2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(getActivityAdvice(25, 15));
-        System.out.println(getActivityAdvice(18, 20));
-        System.out.println(getActivityAdvice(30, 35));
-        System.out.println(getActivityAdvice(50, 5));
-        System.out.println(getActivityAdvice(22, -5));
+        System.out.println(conditionsOfTheProblem(25, 15));
+        System.out.println(conditionsOfTheProblem(18, 20));
+        System.out.println(conditionsOfTheProblem(30, 35));
+        System.out.println(conditionsOfTheProblem(50, 5));
+        System.out.println(conditionsOfTheProblem(31, -5));
+        System.out.println(conditionsOfTheProblem(generateRandomAge(), -30));
+        System.out.println(conditionsOfTheProblem(generateRandomAge(), 25));
     }
 
-    public static String getActivityAdvice(int age, int temperature) {
+    public static String conditionsOfTheProblem(int age, int temperature) {
 
 
         if ((age >= 20 && age <= 40 && temperature >= -20 && temperature <= 30)
@@ -19,9 +23,26 @@ public class Main {
             return "Остовайтесь дома";
         }
     }
+
+    public class OutdoorActivityAdvisor {
+        public static void main(String[] args) {
+
+            // Hay Sensei Aleksey
+
+            System.out.println(conditionsOfTheProblem(23, 10));
+            System.out.println(conditionsOfTheProblem(generateRandomAge(), 10));
+        }
+    }
+
+    public static int generateRandomAge() {
+        Random random = new Random();
+        return random.nextInt(45) + 1;
+    }
 }
 
-             // Hay Sensei
+
+
+
 
 
 
